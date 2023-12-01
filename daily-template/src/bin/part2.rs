dc7dac758ced9d914fa52af1da875512dd270f9c
Johannes Schöpp <darkdiscover@gmail.com>
2023-12-01 16:58:28 +0100

    added inputs to tremplate and corrected path

--- daily-template/src/bin/part2.rs
+++ daily-template/src/bin/part2.rs
@@ -2,7 +2,7 @@ use {{crate_name}}::part2::process;
 use anyhow::Context;
 
 fn main() -> anyhow::Result<()> {
-    let file = include_str!("../../input1.txt");
+    let file = include_str!("../../input2.txt");
     let result = process(file).context("process part 1")?;
     println!("{}", result);
     Ok(())
