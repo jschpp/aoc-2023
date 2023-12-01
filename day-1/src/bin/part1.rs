1a121104cbe9a5ddc088afd9a41164104b4c7fd0
Johannes Schöpp <darkdiscover@gmail.com>
2023-12-01 16:20:49 +0100

    renamed functions

--- day-1/src/bin/part1.rs
+++ day-1/src/bin/part1.rs
@@ -1,9 +1,9 @@
 use anyhow::{Ok, Result};
-use day_1::process_part1;
+use day_1::part1::process;
 
 fn main() -> Result<()> {
     let input = include_str!("../../input.txt");
-    let result = process_part1(input)?;
+    let result = process(input)?;
     println!("{}", result);
     Ok(())
 }
--- day-1/src/bin/part2.rs
+++ day-1/src/bin/part2.rs
@@ -1,9 +1,9 @@
 use anyhow::{Ok, Result};
-use day_1::process_part2;
+use day_1::part1::process;
 
 fn main() -> Result<()> {
     let input = include_str!("../../input.txt");
-    let result = process_part2(input)?;
+    let result = process(input)?;
     println!("{}", result);
     Ok(())
 }
