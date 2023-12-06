657c483ba586961389f8c7a986416f53954ce80b
Johannes Schöpp <darkdiscover@gmail.com>
2023-12-06 19:37:49 +0100

    init day-04

---
+++ day-04/src/bin/part2.rs
@@ -0,0 +1,9 @@
+use day_04::part2::process;
+use anyhow::Context;
+
+fn main() -> anyhow::Result<()> {
+    let file = include_str!("../../input2.txt");
+    let result = process(file).context("process part 1")?;
+    println!("{}", result);
+    Ok(())
+}
