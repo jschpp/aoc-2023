a27e320e63fd4234f4473127b2bec55d88198f8b
Johannes Schöpp <darkdiscover@gmail.com>
2023-12-07 00:51:54 +0100

    day-06

--- day-06/src/part1.rs
+++ day-06/src/part1.rs
@@ -1,7 +1,58 @@
-pub fn process(
-    _input: &str,
-) -> anyhow::Result<String> {
-    todo!("day 01 - part 1");
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, line_ending, multispace1},
+    multi::separated_list1,
+    sequence::{preceded, tuple},
+    IResult,
+};
+
+#[derive(Debug)]
+pub struct Race {
+    pub time: u32,
+    pub distance: u32,
+}
+
+pub fn my_parser(input: &str) -> IResult<&str, Vec<Race>> {
+    let (input, times): (&str, Vec<u32>) = preceded(
+        tuple((tag("Time:"), multispace1)),
+        separated_list1(multispace1, complete::u32),
+    )(input)?;
+    let (input, _) = line_ending(input)?;
+    let (input, distances): (&str, Vec<u32>) = preceded(
+        tuple((tag("Distance:"), multispace1)),
+        separated_list1(multispace1, complete::u32),
+    )(input)?;
+    let races: Vec<Race> = times
+        .iter()
+        .zip(distances)
+        .map(|(&time, distance)| Race { time, distance })
+        .collect();
+    Ok((input, races))
+}
+
+impl Race {
+    pub fn alternatives(&self) -> Vec<u32> {
+        (1..self.time)
+            .into_iter()
+            .map(|hold| (self.time - hold) * hold)
+            .collect::<Vec<u32>>()
+    }
+}
+
+pub fn process(input: &str) -> anyhow::Result<String> {
+    let (_, races) = my_parser(input).unwrap();
+    let result: usize = races
+        .iter()
+        .map(|race| {
+            let alternatives = race.alternatives();
+            alternatives
+                .iter()
+                .filter(|&alternative| *alternative > race.distance)
+                // .inspect(|f| println!("race:{:?}, alt: {:?}", race, f))
+                .count()
+        })
+        .product();
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -10,9 +61,9 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "Time:      7  15   30
+Distance:  9  40  200";
+        assert_eq!("288", process(input)?);
         Ok(())
     }
 }
--- day-06/src/part2.rs
+++ day-06/src/part2.rs
@@ -1,7 +1,42 @@
-pub fn process(
-    _input: &str,
-) -> anyhow::Result<String> {
-    todo!("day 01 - part 1");
+#[derive(Debug)]
+pub struct Race {
+    pub time: usize,
+    pub distance: usize,
+}
+
+impl Race {
+    pub fn alternatives(&self) -> Vec<usize> {
+        (1..self.time)
+            .into_iter()
+            .map(|hold| (self.time - hold) * hold)
+            .collect::<Vec<usize>>()
+    }
+}
+
+fn my_parser(input: &str) -> Race {
+    let r: Vec<usize> = input
+        .lines()
+        .map(|x| {
+            let digit = x.chars().filter(|x| x.is_ascii_digit()).collect::<String>();
+            digit.parse::<usize>().expect("Ascii Digits")
+        })
+        .collect();
+    assert!(r.len() == 2, "wrong parsing");
+    Race {
+        time: r[0],
+        distance: r[1],
+    }
+}
+
+pub fn process(input: &str) -> anyhow::Result<String> {
+    let race = my_parser(input);
+    let alternatives = race.alternatives();
+    let result: usize = alternatives
+        .iter()
+        .filter(|&alternative| *alternative > race.distance)
+        // .inspect(|f| println!("race:{:?}, alt: {:?}", race, f))
+        .count();
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -10,9 +45,9 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "Time:      7  15   30
+Distance:  9  40  200";
+        assert_eq!("71503", process(input)?);
         Ok(())
     }
 }
