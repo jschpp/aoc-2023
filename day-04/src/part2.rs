0a677e2ab63630c574b6d050376f7618e747c836
Johannes Schöpp <darkdiscover@gmail.com>
2023-12-06 21:24:19 +0100

    day 4 part 2

--- day-04/src/part2.rs
+++ day-04/src/part2.rs
@@ -1,7 +1,33 @@
-pub fn process(
-    _input: &str,
-) -> anyhow::Result<String> {
-    todo!("day 01 - part 1");
+use super::my_parser::*;
+use std::collections::VecDeque;
+
+pub fn process(input: &str) -> anyhow::Result<String> {
+    let (_input, cards) = my_parser(input).unwrap();
+    let max_cards = cards.len() as u32;
+    let mut cards_touched: u32 = 0;
+    let mut todo: VecDeque<Card> = cards.clone().into();
+
+    //TODO: this is horrifically slow... Need to cache this sometime.
+    while !todo.is_empty() {
+        let card = todo
+            .pop_front()
+            .expect("since todo is not yet empty this must succeed");
+        cards_touched += 1;
+        let num_correct = card
+            .numbers
+            .iter()
+            .filter(|number| card.winning.contains(number))
+            .count() as u32;
+        if num_correct > 0 {
+            let r = max_cards.min(card.number + 1)..=max_cards.min(card.number + num_correct);
+            // dbg!(&r, card.number);
+            cards
+                .iter()
+                .filter(|x| r.contains(&x.number))
+                .for_each(|x| todo.push_back(x.clone()));
+        }
+    }
+    Ok(cards_touched.to_string())
 }
 
 #[cfg(test)]
@@ -10,9 +36,14 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+";
+        assert_eq!("30", process(input)?);
         Ok(())
     }
 }
