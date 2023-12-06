8134d110cdf0f039439702db5e095825fbe5924b
Johannes Schöpp <darkdiscover@gmail.com>
2023-12-06 19:35:55 +0100

    day 3 part 2

--- day-03/src/bin/part2.rs
+++ day-03/src/bin/part2.rs
@@ -2,7 +2,7 @@ use day_03::part2::process;
 use anyhow::Context;
 
 fn main() -> anyhow::Result<()> {
-    let file = include_str!("../../input2.txt");
+    let file = include_str!("../../input.txt");
     let result = process(file).context("process part 1")?;
     println!("{}", result);
     Ok(())
--- day-03/src/part2.rs
+++ day-03/src/part2.rs
@@ -1,7 +1,48 @@
-pub fn process(
-    _input: &str,
-) -> anyhow::Result<String> {
-    todo!("day 01 - part 1");
+use super::my_parser::{line_parser, CharPart, Parts, ValuePart};
+
+pub fn process(input: &str) -> anyhow::Result<String> {
+    let parts: Vec<Parts> = input
+        .lines()
+        .enumerate()
+        .map(|(line_number, line)| line_parser(line, line_number))
+        .collect();
+    // select 'gears'
+    let char_parts: Vec<CharPart> =
+        parts
+            .iter()
+            .map(|x| x.char_parts.clone())
+            .fold(Vec::new(), |mut v: Vec<CharPart>, x| {
+                v.extend(x.iter().filter(|part| part.value == '*'));
+                v
+            });
+    // select all value parts touching a gear
+    let value_parts: Vec<ValuePart> = parts
+        .iter()
+        .map(|x| x.value_parts.clone())
+        .fold(Vec::new(), |mut v: Vec<ValuePart>, x| {
+            v.extend(x.iter());
+            v
+        })
+        .into_iter()
+        .filter(|part| part.touches(&char_parts))
+        .collect();
+    // for all 'gears' check if they touch exactly 2 value parts and calculate the gear ratio
+    let result: usize = char_parts
+        .iter()
+        .filter_map(|gear| {
+            let touching: Vec<ValuePart> = value_parts
+                .iter()
+                .copied()
+                .filter(|part| part.touches(&[*gear]))
+                .collect();
+            if touching.len() == 2 {
+                Some(touching[0].value * touching[1].value)
+            } else {
+                None
+            }
+        })
+        .sum();
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -10,9 +51,17 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+        assert_eq!("467835", process(input)?);
         Ok(())
     }
 }
