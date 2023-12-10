6a4849555798dfbfaab5e1e1a458db45dd7dbef0
Johannes Schöpp <darkdiscover@gmail.com>
2023-12-10 12:20:47 +0100

    clenup template

--- daily-template/src/bin/part2.rs
+++ daily-template/src/bin/part2.rs
@@ -2,6 +2,6 @@ use {{crate_name}}::part2::process;
 
 fn main() {
     let file = include_str!("../../input.txt");
-    let result = process(file).context("process part 2")?;
+    let result = process(file);
     println!("{}", result);
 }
