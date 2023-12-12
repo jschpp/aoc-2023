6c4e17e5976cdeb7dea7828bdb2f773b79a964c7
Johannes Schöpp <darkdiscover@gmail.com>
2023-12-12 16:00:30 +0100

    f this day-12

--- day-12/src/part1.rs
+++ day-12/src/part1.rs
@@ -1,5 +1,253 @@
-pub fn process(_input: &str) -> String {
-    todo!("day 01 - part 1");
+use std::fmt::Debug;
+
+use itertools::{repeat_n, Itertools};
+
+#[derive(Debug, Clone, Copy)]
+pub struct Position {
+    pub line: usize,
+    pub column: usize,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum RecordType {
+    Broken,
+    Unknown,
+    Empty,
+}
+
+impl TryFrom<char> for RecordType {
+    type Error = &'static str;
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            '#' => Ok(RecordType::Broken),
+            '?' => Ok(RecordType::Unknown),
+            '.' => Ok(RecordType::Empty),
+            _ => Err("unknown type"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Record {
+    pub typ: RecordType,
+    pub position: Position,
+}
+
+#[derive(Debug)]
+pub struct Configuration {
+    pub groups: Vec<usize>,
+}
+
+#[derive(Debug)]
+pub struct Line {
+    pub records: Vec<Record>,
+    pub configuration: Vec<usize>,
+}
+
+pub fn parse_record(input: &str, line_idx: usize) -> Vec<Record> {
+    input
+        .chars()
+        .enumerate()
+        .map(|(char_idx, c)| Record {
+            typ: c.try_into().expect("should work"),
+            position: Position {
+                line: line_idx,
+                column: char_idx,
+            },
+        })
+        .collect()
+}
+
+pub fn line_parser(input: &str, line_idx: usize) -> Line {
+    let parts = input.split(' ').collect::<Vec<&str>>();
+    Line {
+        records: parse_record(parts[0], line_idx),
+        configuration: parts[1]
+            .split(',')
+            .flat_map(|x| x.parse::<usize>().ok())
+            .collect(),
+    }
+}
+
+fn get_options_for_unknown(len: usize, offset: usize) -> Vec<Vec<(usize, RecordType)>> {
+    repeat_n(vec![RecordType::Broken, RecordType::Empty].into_iter(), len)
+        .multi_cartesian_product()
+        .unique()
+        .map(|v| {
+            v.into_iter()
+                .enumerate()
+                .map(|(idx, elem)| (idx + offset, elem))
+                .collect::<Vec<(usize, RecordType)>>()
+        })
+        .filter(|v| v.len() == len)
+        // .inspect(|x| println!("{:?}", x))
+        .collect()
+}
+
+// fn pretty_print(records: &[Record]) {
+//     for record in records {
+//         match record.typ {
+//             RecordType::Broken => print!("#"),
+//             RecordType::Empty => print!("."),
+//             RecordType::Unknown => print!("?"),
+//         }
+//     }
+//     println!()
+// }
+
+fn new_line(
+    old_line: &[Record],
+    options_for_unknown: Vec<Vec<(usize, RecordType)>>,
+    configuration: &[usize],
+) -> usize {
+    assert!(!old_line.is_empty());
+    // let mut options: Vec<Vec<Record>> = Vec::with_capacity(options_for_unknown.len() * 2);
+    let mut count: usize = 0;
+    let line_number = old_line[0].position.line;
+    for v in options_for_unknown {
+        let mut tmp_rec = old_line.to_vec();
+        for (idx, option) in v {
+            tmp_rec[idx] = Record {
+                position: Position {
+                    line: line_number,
+                    column: idx,
+                },
+                typ: option,
+            };
+        }
+        if filter_option(configuration, &tmp_rec) {
+            count += 1
+        }
+    }
+    count
+}
+
+pub fn filter_option(configuration: &[usize], options: &[Record]) -> bool {
+    let mut ranges: Vec<usize> = Vec::new();
+    let mut range_count: usize = 0;
+    for record in options {
+        match record.typ {
+            RecordType::Empty => {
+                if range_count != 0 {
+                    ranges.push(range_count);
+                    range_count = 0;
+                }
+            }
+            RecordType::Broken => range_count += 1,
+            RecordType::Unknown => panic!("shouldn't exist here any longer"),
+        }
+    }
+    if range_count != 0 {
+        ranges.push(range_count);
+    }
+    // println!("conf: {:?} seen: {:?}", self.configuration, ranges);
+    configuration == ranges
+}
+
+/// append each new set of unknowns to each existing set
+/// this will do this:
+///
+/// ```rust
+/// use crate::day_12::part1::RecordType;
+///
+/// let original_unknowns = [[(0,RecordType::Unknown)], [(0,RecordType::Broken)]];
+/// let new_unknowns = [[(1,RecordType::Unknown)],[(0,RecordType::Broken)]];
+/// let result_of_merge_options =[
+///     [(0,RecordType::Unknown), (1,RecordType::Unknown)],
+///     [(0,RecordType::Unknown),(1,RecordType::Broken)],
+///     [(0,RecordType::Broken),(1,RecordType::Unknown)],
+///     [(0,RecordType::Broken),(1,RecordType::Broken)]
+/// ];
+/// ```
+fn merge_options(
+    original: &[Vec<(usize, RecordType)>],
+    options: &[Vec<(usize, RecordType)>],
+) -> Vec<Vec<(usize, RecordType)>> {
+    if original.is_empty() {
+        // first time finding unknowns
+        options.to_vec()
+    } else {
+        let new_options: Vec<Vec<(usize, RecordType)>> = options
+            .iter()
+            // .inspect(|x| println!("{:?}", x))
+            .flat_map(|unknown_option| {
+                original
+                    .iter()
+                    // .inspect(|x| println!("{:?}", x))
+                    .map(|original_options| {
+                        let mut unknown_option = unknown_option.clone();
+                        let mut original_options = original_options.clone();
+                        original_options.append(&mut unknown_option);
+                        original_options
+                    })
+                    .collect::<Vec<Vec<(usize, RecordType)>>>()
+            })
+            // .inspect(|x| println!("{:?}", x))
+            .collect();
+        new_options
+    }
+}
+
+impl Line {
+    pub fn find_options(&self) -> usize {
+        let mut unknown_range: Vec<usize> = Vec::new();
+        let mut options: Vec<Vec<(usize, RecordType)>> = Vec::new();
+        for record in self.records.iter() {
+            match record.typ {
+                RecordType::Unknown => unknown_range.push(record.position.column),
+                _ => {
+                    if unknown_range.is_empty() {
+                        // no new unknowns found
+                        continue;
+                    }
+
+                    let options_for_unknown = get_options_for_unknown(
+                        unknown_range.len(),
+                        record.position.column - unknown_range.len(),
+                    );
+
+                    if !unknown_range.is_empty() {
+                        unknown_range.clear();
+                    }
+
+                    options = merge_options(&options, &options_for_unknown);
+                }
+            };
+        }
+        if !unknown_range.is_empty() {
+            println!("here");
+            let options_for_unknown = get_options_for_unknown(
+                unknown_range.len(),
+                self.records.len() - unknown_range.len(),
+            );
+            options = merge_options(&options, &options_for_unknown);
+        }
+
+        let new_line_options: usize = new_line(&self.records, options, &self.configuration);
+        // new_line_options.iter().for_each(|o| pretty_print(o));
+        println!("{:?}", new_line_options);
+        new_line_options
+    }
+
+    pub fn calculate_options(&self) -> usize {
+        self.find_options()
+        // .iter()
+        // .inspect(|x| pretty_print(x))
+        // .filter(|option| self.filter_option(option))
+        // .inspect(|x| pretty_print(x))
+        // .count()
+    }
+}
+
+pub fn process(input: &str) -> String {
+    input
+        .lines()
+        .enumerate()
+        .map(|(line_idx, line)| line_parser(line, line_idx))
+        .map(|line| line.calculate_options())
+        .sum::<usize>()
+        .to_string()
 }
 
 #[cfg(test)]
@@ -7,8 +255,38 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_options() {
+        let input = "???.#?# 1,1,3";
+        assert_eq!("1", process(input));
+    }
+
+    #[test]
+    fn test_options_1() {
+        let input = ".??..??...?##. 1,1,3";
+        assert_eq!("4", process(input));
+    }
+
+    // #[test]
+    // fn test_options_2() {
+    //     let input = "?????????? 1,1";
+    //     assert_eq!("45", process(input));
+    // }
+
+    #[test]
     fn test_process() {
-        let input = "";
-        assert_eq!("", process(input));
+        let input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1";
+        assert_eq!("21", process(input));
+    }
+
+    #[test]
+    // #[ignore = "slow"]
+    fn test_input() {
+        let input = include_str!("../input.txt");
+        assert_eq!("7771", process(input));
     }
 }
--- day-12/src/part2.rs
+++ day-12/src/part2.rs
@@ -1,14 +1,145 @@
-pub fn process(_input: &str) -> String {
-    todo!("day 01 - part 2");
+use itertools::{repeat_n, Itertools};
+use memoize::memoize;
+
+/// since part1 was waaaaaaaaaaay to slow to work
+///
+/// this on is recursive. It's still not fast but
+/// at least this doesn't crash my pc oO
+/// welp memoize fixes the slow oO'
+#[memoize]
+pub fn count(input: String, nums: Vec<usize>) -> usize {
+    if input.is_empty() {
+        if nums.is_empty() {
+            return 1;
+        }
+        // no more gears but still some needed
+        return 0;
+    }
+
+    if nums.is_empty() {
+        if input.contains('#') {
+            // no more numbers but still broken gears
+            return 0;
+        }
+        return 1;
+    }
+
+    let mut result: usize = 0;
+
+    let first_char = input.chars().next().expect("input not empty");
+
+    if ".?".contains(first_char) {
+        // treat the ? as a . and remove it
+        result += count(input.chars().skip(1).collect::<String>(), nums.clone())
+    }
+
+    if "#?".contains(first_char)
+        && nums[0] <= input.len() // input is longer than next number
+        && !input[..nums[0]].contains('.') // next num chars contain a . so that will be an invalid range
+        && (
+            nums[0] == input.len() // we have exactly num[0] tokens left
+            || input.chars().nth(nums[0]).expect("exists") != '#' // the next token after this block must not be a #.
+                                                                        // There should be a new block after this one
+        )
+    {
+        result += count(
+                // skipping nums[0] + 1 tokens will either
+                // skip the beginning of the next block as to not open a new one when parsing a dot
+                // empty the existing slice in the case of nums[0] == input.len()
+            input.chars().skip(nums[0] + 1).collect::<String>(),
+            nums[1..].to_vec(),
+        );
+    }
+    result
+}
+
+pub fn line_parser(input: String) -> (String, Vec<usize>) {
+    input
+        .split(' ')
+        .tuple_windows::<(_, _)>()
+        .map(|(puzzle, nums)| {
+            (
+                puzzle.to_owned(),
+                nums.split(',')
+                    .map(|n| n.parse::<usize>().expect("parseable"))
+                    .collect(),
+            )
+        })
+        .take(1)
+        .exactly_one()
+        .unwrap()
+}
+
+pub fn unfold(input: &str) -> String {
+    #![allow(unstable_name_collisions)]
+    // ignore intersperse warnings
+    let parts = input.split(' ').collect::<Vec<&str>>();
+    [
+        repeat_n(parts[0], 5).intersperse("?").collect::<String>(),
+        repeat_n(parts[1], 5).intersperse(",").collect::<String>(),
+    ]
+    .join(" ")
+}
+
+pub fn process(input: &str) -> String {
+    let lines: Vec<(String, Vec<usize>)> = input
+        .lines()
+        .map(unfold)
+        .map(line_parser)
+        // .inspect(|x| println!("{:?}", x))
+        .collect();
+    let mut result = 0;
+    for line in lines.iter() {
+        result += count(line.0.clone(), line.1.clone());
+    }
+    result.to_string()
 }
 
 #[cfg(test)]
 mod tests {
+    use rstest::rstest;
     use super::*;
 
     #[test]
+    fn test_unfold() {
+        let input = "???.### 1,1,3";
+        assert_eq!(
+            "???.###????.###????.###????.###????.### 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3",
+            unfold(input)
+        );
+
+        let input = ".# 1";
+        assert_eq!(".#?.#?.#?.#?.# 1,1,1,1,1", unfold(input));
+    }
+
+
+    
+    #[rstest]
+    #[case("???.### 1,1,3", "1")]
+    #[case(".??..??...?##. 1,1,3", "16384")]
+    #[case("?#?#?#?#?#?#?#? 1,3,1,6", "1")]
+    #[case("????.#...#... 4,1,1", "16")]
+    #[case("????.######..#####. 1,6,5", "2500")]
+    #[case("?###???????? 3,2,1", "506250")]
+    fn test_single_line(#[case] input: String, #[case] expected: String) {
+        assert_eq!(process(&input), expected);
+    }
+
+    #[test]
     fn test_process() {
-        let input = "";
-        assert_eq!("", process(input));
+        let input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1";
+        assert_eq!("525152", process(input));
+    }
+
+    #[test]
+    #[ignore = "slowish"]
+    fn test_input() {
+        let input = include_str!("../input.txt");
+        assert_eq!("10861030975833", process(input));
     }
 }
