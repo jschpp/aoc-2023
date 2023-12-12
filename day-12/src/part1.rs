6efbb8db23782e803879e94cafe2636c5c4f81bd
Johannes Schöpp <darkdiscover@gmail.com>
2023-12-12 05:42:41 +0100

    init day-12

---
+++ day-12/src/part1.rs
@@ -0,0 +1,14 @@
+pub fn process(_input: &str) -> String {
+    todo!("day 01 - part 1");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let input = "";
+        assert_eq!("", process(input));
+    }
+}
---
+++ day-12/src/part2.rs
@@ -0,0 +1,14 @@
+pub fn process(_input: &str) -> String {
+    todo!("day 01 - part 2");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let input = "";
+        assert_eq!("", process(input));
+    }
+}
