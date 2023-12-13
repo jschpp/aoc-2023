06103508687bf4cb0970511d7bb79549d52663dd
Johannes Schöpp <darkdiscover@gmail.com>
2023-12-13 16:10:00 +0100

    day-13

--- day-13/src/part2.rs
+++ day-13/src/part2.rs
@@ -1,5 +1,19 @@
-pub fn process(_input: &str) -> String {
-    todo!("day 01 - part 2");
+use super::shared::*;
+
+pub fn process(input: &str) -> String {
+    let (_, lines) = parse_input(input).unwrap();
+    let mut result: usize = 0;
+    for block in lines.iter() {
+        // rows
+        // dbg!(block);
+        result += find_symmetry(block, 1) * 100;
+
+        // columns
+        let new_block = rotate(block);
+        // dbg!("rotated block", &new_block);
+        result += find_symmetry(&new_block, 1);
+    }
+    result.to_string()
 }
 
 #[cfg(test)]
@@ -8,8 +22,27 @@ mod tests {
 
     #[test]
     fn test_process() {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input));
+        let input = "#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#";
+        assert_eq!("400", process(input));
+    }
+
+    #[test]
+    fn test_input() {
+        let input = include_str!("../input.txt");
+        assert_eq!("25450", process(input));
     }
 }
