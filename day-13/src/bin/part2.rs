823a059be909f9eadbc184e3c5e2e733e2c51324
Johannes Schöpp <darkdiscover@gmail.com>
2023-12-13 17:09:33 +0100

    memory profiling before

--- day-13/src/bin/part2.rs
+++ day-13/src/bin/part2.rs
@@ -1,6 +1,13 @@
 use day_13::part2::process;
 
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
 fn main() {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
     let file = include_str!("../../input.txt");
     let result = process(file);
     println!("{}", result);
