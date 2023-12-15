ce491040cbf3d5346a337f4962dbc8399fd0f284
Johannes Schöpp <darkdiscover@gmail.com>
2023-12-15 13:35:01 +0100

    day-15

--- day-15/src/part2.rs
+++ day-15/src/part2.rs
@@ -1,5 +1,124 @@
-pub fn process(_input: &str) -> String {
-    todo!("day 01 - part 2");
+use std::{collections::HashMap, fmt::Display};
+
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{self, alpha1},
+    combinator::value,
+    multi::separated_list1,
+    sequence::tuple,
+    IResult, Parser,
+};
+
+use super::shared::my_hash;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Operation {
+    Set,
+    Remove,
+}
+
+impl Display for Operation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let x: char = match self {
+            Operation::Remove => '-',
+            Operation::Set => '=',
+        };
+        write!(f, "{}", x)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Instruction {
+    pub label: String,
+    pub op: Operation,
+    pub focal_len: Option<usize>,
+    hash: usize,
+}
+
+impl Instruction {
+    pub fn hash(&self) -> usize {
+        self.hash
+    }
+}
+
+fn parse_set(input: &str) -> IResult<&str, Instruction> {
+    tuple((alpha1, value(Operation::Set, tag("=")), complete::u32))
+        .parse(input)
+        .map(|(rest, (name, op, val))| {
+            (
+                rest,
+                Instruction {
+                    focal_len: Some(val as usize),
+                    hash: my_hash(name) as usize,
+                    label: name.to_string(),
+                    op,
+                },
+            )
+        })
+}
+
+fn parse_remove(input: &str) -> IResult<&str, Instruction> {
+    tuple((alpha1, value(Operation::Remove, tag("-"))))
+        .parse(input)
+        .map(|(rest, (name, op))| {
+            (
+                rest,
+                Instruction {
+                    label: name.to_string(),
+                    op,
+                    focal_len: None,
+                    hash: my_hash(name) as usize,
+                },
+            )
+        })
+}
+
+fn my_parser(input: &str) -> IResult<&str, Vec<Instruction>> {
+    separated_list1(tag(","), alt((parse_remove, parse_set)))(input)
+}
+
+pub fn process(input: &str) -> String {
+    let (_, parsed) = my_parser(input).unwrap();
+
+    let mut boxes: Vec<Vec<String>> = vec![Vec::new(); 256];
+    let mut focal_lengths: HashMap<String, usize> = HashMap::new();
+
+    parsed.iter().for_each(|instruction| {
+        let idx = boxes[instruction.hash()]
+            .iter()
+            .position(|l| l == &instruction.label);
+        match instruction.op {
+            Operation::Set => {
+                if idx.is_none() {
+                    boxes[instruction.hash()].push(instruction.label.clone());
+                }
+                let val = instruction.focal_len.expect("value should exist");
+                focal_lengths
+                    .entry(instruction.label.clone())
+                    .and_modify(|x| *x = val)
+                    .or_insert(val);
+            }
+            Operation::Remove => {
+                if let Some(idx) = idx {
+                    boxes[instruction.hash()].remove(idx);
+                }
+            }
+        }
+    });
+
+    let result = boxes
+        .into_iter()
+        .zip(1..)
+        .fold(0, |acc, (lens_box, box_number)| {
+            acc + lens_box
+                .into_iter()
+                .zip(1..)
+                .fold(0, |acc, (lens, lens_number)| {
+                    acc + box_number * lens_number * focal_lengths[&lens]
+                })
+        });
+    result.to_string()
 }
 
 #[cfg(test)]
@@ -7,9 +126,71 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_remove() {
+        let input = "cm-";
+        let (rest, parsed) = parse_remove(input).unwrap();
+        assert_eq!(rest.len(), 0);
+        assert_eq!(
+            Instruction {
+                focal_len: None,
+                hash: my_hash("cm") as usize,
+                label: "cm".to_string(),
+                op: Operation::Remove
+            },
+            parsed
+        )
+    }
+
+    #[test]
+    fn test_set() {
+        let input = "rn=1";
+        let (rest, parsed) = parse_set(input).unwrap();
+        assert_eq!(rest.len(), 0);
+        assert_eq!(
+            Instruction {
+                focal_len: Some(1),
+                hash: my_hash("rn") as usize,
+                label: "rn".to_string(),
+                op: Operation::Set
+            },
+            parsed
+        )
+    }
+
+    #[test]
+    fn test_parser() {
+        let input = "rn=1,cm-";
+        let result: Vec<Instruction> = vec![
+            Instruction {
+                focal_len: Some(1),
+                hash: my_hash("rn") as usize,
+                label: "rn".to_owned(),
+                op: Operation::Set,
+            },
+            Instruction {
+                focal_len: None,
+                hash: my_hash("cm") as usize,
+                label: "cm".to_owned(),
+                op: Operation::Remove,
+            },
+        ];
+        let parsed = match my_parser(input) {
+            Ok((_rest, parsed)) => parsed,
+            Err(e) => panic!("{}", e),
+        };
+        assert_eq!(result, parsed);
+    }
+
+    #[test]
     fn test_process() {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+        assert_eq!("145", process(input));
+    }
+
+    #[test]
+    #[ignore = "noisy"]
+    fn test_input() {
+        let input = include_str!("../input.txt");
+        assert_eq!("303404", process(input));
     }
 }
---
+++ day-15/src/shared.rs
@@ -0,0 +1,27 @@
+pub fn my_hash(input: &str) -> u32 {
+    input
+        .chars()
+        .fold(0, |acc, c| ((acc + c as u32) * 17) % 256)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("rn=1", 30)]
+    #[case("cm-", 253)]
+    #[case("qp=3", 97)]
+    #[case("cm=2", 47)]
+    #[case("qp-", 14)]
+    #[case("pc=4", 180)]
+    #[case("ot=9", 9)]
+    #[case("ab=5", 197)]
+    #[case("pc-", 48)]
+    #[case("pc=6", 214)]
+    #[case("ot=7", 231)]
+    fn test_single_hash(#[case] input: String, #[case] expected: u32) {
+        assert_eq!(my_hash(&input), expected);
+    }
+}
