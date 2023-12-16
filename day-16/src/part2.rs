d146e37ff26ae4e26ce0e1a9289a6d6b90212a3a
Johannes Schöpp <darkdiscover@gmail.com>
2023-12-16 14:33:31 +0100

    day-16 part2 donw

--- day-16/src/part2.rs
+++ day-16/src/part2.rs
@@ -1,5 +1,101 @@
-pub fn process(_input: &str) -> String {
-    todo!("day 01 - part 2");
+use super::shared::*;
+use glam::IVec2;
+use std::collections::{HashMap, VecDeque};
+use toodee::{TooDee, TooDeeOps};
+
+fn illuminate_grid(grid: &mut TooDee<Tile>, start: (IVec2, Direction)) -> usize {
+    let mut work_queue: VecDeque<(IVec2, Direction)> = VecDeque::new();
+    work_queue.push_back(start);
+    while !work_queue.is_empty() {
+        let (pos, to) = work_queue.pop_front().expect("not empty yet");
+        if let Some(new_positions) = grid[pos.x as usize][pos.y as usize].pass(to) {
+            for new_pos in new_positions.into_iter() {
+                if new_pos.x >= 0 && new_pos.y >= 0 {
+                    let x = new_pos.x as usize;
+                    let y = new_pos.y as usize;
+                    let new_direction: Direction = Direction::from_points(pos, new_pos);
+                    if x < grid.num_cols() && y < grid.num_rows() {
+                        work_queue.push_back((new_pos, new_direction))
+                    }
+                }
+            }
+        }
+    }
+    grid.into_iter().filter(|tile| tile.illuminated).count()
+}
+
+fn get_starting_positions(num_rows: i32, num_cols: i32) -> Vec<(IVec2, Direction)> {
+    assert!(num_cols >= 0 && num_rows >= 0);
+    // convert to idx
+    let max_row_idx = num_rows - 1;
+    let max_col_idx = num_cols - 1;
+
+    // corners
+    let mut result: Vec<(IVec2, Direction)> = vec![
+        (IVec2::new(0, 0), Direction::East),
+        (IVec2::new(0, 0), Direction::South),
+        (IVec2::new(max_col_idx, 0), Direction::West),
+        (IVec2::new(max_col_idx, 0), Direction::South),
+        (IVec2::new(0, max_row_idx), Direction::East),
+        (IVec2::new(0, max_row_idx), Direction::North),
+        (IVec2::new(max_col_idx, 0), Direction::West),
+        (IVec2::new(max_col_idx, 0), Direction::North),
+    ];
+
+    // left & right edges
+    for y_pos in 1..max_row_idx {
+        result.push((IVec2::new(0, y_pos), Direction::East));
+        result.push((IVec2::new(max_col_idx, y_pos), Direction::West));
+    }
+
+    // top and bottom row
+    for x_pos in 1..max_col_idx {
+        result.push((IVec2::new(x_pos, 0), Direction::South));
+        result.push((IVec2::new(x_pos, max_row_idx), Direction::North));
+    }
+
+    result
+}
+
+pub fn process(input: &str) -> String {
+    let mut rows: usize = 0;
+    let mut cols: usize = 0;
+    let tiles: HashMap<IVec2, Tile> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(line_idx, line)| {
+            rows = rows.max(line_idx + 1);
+            line.chars()
+                .enumerate()
+                .map(|(c_idx, c)| {
+                    cols = cols.max(c_idx + 1);
+                    let pos = IVec2::new(c_idx as i32, line_idx as i32);
+                    (
+                        pos,
+                        Tile::new(
+                            pos,
+                            match c {
+                                '.' => None,
+                                val => Some(val),
+                            },
+                        ),
+                    )
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect();
+    let mut grid: TooDee<Tile> = TooDee::new(cols, rows);
+    for x in 0..cols {
+        for y in 0..rows {
+            grid[x][y] = *tiles.get(&IVec2::new(x as i32, y as i32)).expect("exists")
+        }
+    }
+    get_starting_positions(rows as i32, cols as i32)
+        .iter()
+        .map(|s| illuminate_grid(&mut grid.clone(), *s))
+        .max()
+        .expect("some result should exists")
+        .to_string()
 }
 
 #[cfg(test)]
@@ -8,8 +104,22 @@ mod tests {
 
     #[test]
     fn test_process() {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input));
+        let input = r".|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|....";
+        assert_eq!("51", process(input));
+    }
+
+    #[test]
+    fn test_input() {
+        let input = include_str!("../input.txt");
+        assert_eq!("7846", process(input));
     }
 }
