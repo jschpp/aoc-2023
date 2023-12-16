300c1e8d4d314d0eacde09d5a1d86bb0fd00e83d
Johannes Schöpp <darkdiscover@gmail.com>
2023-12-16 14:35:44 +0100

    fix typo in template

--- daily-template/src/part2.rs
+++ daily-template/src/part2.rs
@@ -10,6 +10,6 @@ mod tests {
     fn test_process() {
         todo!("haven't built test yet");
         let input = "";
-        assert_eq!("", process(input)?);
+        assert_eq!("", process(input));
     }
 }
